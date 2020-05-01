f810fa708b0fd4827bfdb1f467f4b1c858420c8e
the_w <the_w@192.168.1.52>
2020-05-01 18:27:23 +0200

    Check Style

--- src/main/java/utils/MailService.java
+++ src/main/java/utils/MailService.java
@@ -5,7 +5,6 @@ import java.net.URISyntaxException;
 
 import microsoft.exchange.webservices.data.core.ExchangeService;
 import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion;
-import microsoft.exchange.webservices.data.credential.ExchangeCredentials;
 import microsoft.exchange.webservices.data.credential.WebCredentials;
 
 public class MailService extends ExchangeService{
@@ -13,10 +12,7 @@ public class MailService extends ExchangeService{
 	public MailService(String serverMail, String mailEmetteur, String password) throws URISyntaxException {
 		super(ExchangeVersion.Exchange2010_SP1);
 		this.setUrl(new URI(serverMail));
-		
-        ExchangeCredentials credentials = new WebCredentials(mailEmetteur, password);
-        this.setCredentials(credentials);
-        
+        this.setCredentials(new WebCredentials(mailEmetteur, password));
         this.setTraceEnabled(true);
 	}
 }
