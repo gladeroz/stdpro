01b0af7dc8383a6cd281859d4ee5b9c824d9e980
the_w <the_w@192.168.1.52>
2020-05-01 18:26:54 +0200

    add version Exchange2010

--- src/main/java/utils/MailService.java
+++ src/main/java/utils/MailService.java
@@ -4,15 +4,14 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 import microsoft.exchange.webservices.data.core.ExchangeService;
+import microsoft.exchange.webservices.data.core.enumeration.misc.ExchangeVersion;
 import microsoft.exchange.webservices.data.credential.ExchangeCredentials;
 import microsoft.exchange.webservices.data.credential.WebCredentials;
 
 public class MailService extends ExchangeService{
 
 	public MailService(String serverMail, String mailEmetteur, String password) throws URISyntaxException {
-		super();
-		
-		//service = new ExchangeService(ExchangeVersion.Exchange2010_SP1);
+		super(ExchangeVersion.Exchange2010_SP1);
 		this.setUrl(new URI(serverMail));
 		
         ExchangeCredentials credentials = new WebCredentials(mailEmetteur, password);
