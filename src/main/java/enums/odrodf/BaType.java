97d8d623a2b0d03380524ff43d2b2524301916da
the_w <the_w@DESKTOP-9B98EEF.home>
2020-10-24 12:23:06 +0200

    [FEATURE] Add new FORM type + ODF on 5 years

---
+++ src/main/java/enums/odrodf/BaType.java
@@ -0,0 +1,26 @@
+package enums.odrodf;
+
+import enums.OdrType;
+
+public enum BaType {
+	S(OdrType.S),
+	
+	NV(OdrType.NV),
+	
+	NS(OdrType.NS),
+	NS_RES(OdrType.NS),
+	NS_ODR_HD(OdrType.NS),
+	NS_ODF_HD(OdrType.NS),
+	NS_ODF_AT(OdrType.NS),
+	NS_NOT_ELI(OdrType.NS);
+	
+	public final OdrType output;
+	 
+    private BaType(OdrType output) {
+        this.output = output;
+    }
+
+	public OdrType getOutput() {
+		return output;
+	}
+}
