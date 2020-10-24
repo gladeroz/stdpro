5f7bac983521db315144bad150d5df6770c3d6a1
the_w <the_w@DESKTOP-9B98EEF.home>
2020-10-24 12:23:06 +0200

    [FEATURE] Add new FORM type + ODF on 5 years

---
+++ src/main/java/enums/odrodf/RibType.java
@@ -0,0 +1,17 @@
+package enums.odrodf;
+
+import enums.OdrType;
+
+public enum RibType {
+	S(OdrType.S);
+	
+	public final OdrType output;
+	 
+    private RibType(OdrType output) {
+        this.output = output;
+    }
+
+	public OdrType getOutput() {
+		return output;
+	}
+}
